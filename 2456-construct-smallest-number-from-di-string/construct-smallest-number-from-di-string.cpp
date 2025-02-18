9a4f01ece5afd2225d3b9a755113d56f0685fec0
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-02-18 10:43:59 +0530

    Time: 56 ms (6.09%) | Memory: 7.7 MB (85.60%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -9,29 +9,16 @@ public:
         }
         return true;
     }
-    void permutations(string pattern,string s,vector<string>&valid,int idx,int n){
-        if(idx==n){
-            if(check(pattern,s))
-            {
-                valid.push_back(s);
-                return;
-            }
-        }
-        for(int i=idx;i<n;i++){
-            swap(s[i],s[idx]);
-            permutations(pattern,s,valid,idx+1,n);
-            swap(s[i],s[idx]);
-        }
-    }
     string smallestNumber(string pattern) {
         string s;
         int n=pattern.size();
         for(int i=1;i<=n+1;i++){
             s=s+char('0'+i);
         }
-        vector<string>valid;
-        permutations(pattern,s,valid,0,n+1);
-        
-        return valid[0];
+        do {
+            if(check(pattern,s))
+            return s;
+        } while (next_permutation(s.begin(), s.end()));
+        return "";
     }
 };
