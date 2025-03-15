4f14a91099112d6c46353e26a66612971aed4536
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-03-15 13:55:36 +0530

    Time: 51 ms (55.30%) | Memory: 29.2 MB (48.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int k=0;k<nums.size();k++){
+            if(k>0 && nums[k]==nums[k-1])
+            continue;
+            int i=k+1,j=nums.size()-1;
             
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+            while(i<j){
+                int sum=nums[k]+nums[i]+nums[j];
+                // cout<<sum<<endl;
+                if(sum==0){
+                    // if(find(ans.begin(), ans.end(), vector<int> {nums[k],nums[i],nums[j]}) == ans.end())
+                    ans.push_back({nums[k],nums[i],nums[j]});
+                    i++;
+                    while(nums[i]==nums[i-1]&&i<j)
+                    i++;
                 }
+                else if(sum<0)
+                i++;
+                else
+                j--;
             }
         }
-        return res;        
+        return ans;
     }
 };
