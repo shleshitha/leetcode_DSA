55c1a0355118f34aba5c7fe31e2d1a46ca283829
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-08-19 19:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 111.4 MB (40.61%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans=0,cnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                while(i<nums.size()&&nums[i]==0){
-                    cnt++;
-                    i++;
-                }
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==0&&nums[i+1]!=0){
+                cnt++;
                 // cout<<cnt<<endl;
                 ans+=(cnt*(cnt+1))/2;
                 cnt=0;
-                i--;
             }
+            else if(nums[i]==0)
+            cnt++;
+            
+        }
+        if(nums[nums.size()-1]==0){
+            cnt++;
+            ans+=(cnt*(cnt+1))/2;
         }
         return ans;
     }
