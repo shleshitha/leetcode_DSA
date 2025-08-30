eed4eda675dba4fb41cf2864f76d4d9ba70f0dca
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-08-30 10:27:33 +0530

    Time: 8 ms (26.88%) | Memory: 23.3 MB (59.53%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -3,26 +3,20 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         vector<vector<int>>rows(9,vector<int>(9,0));
         vector<vector<int>>cols(9,vector<int>(9,0));
-        vector<vector<int>>x={
-            {0,0,2,2},{0,3,2,5},{0,6,2,8},
-            {3,0,5,2},{3,3,5,5},{3,6,5,8},
-            {6,0,8,2},{6,3,8,5},{6,6,8,8}
-        };
-        for(int grid=0;grid<9;grid++){
-            vector<bool>t(10,false);
-            for(int i=x[grid][0];i<=x[grid][2];i++){
-                for(int j=x[grid][1];j<=x[grid][3];j++){
-                    if(board[i][j]=='.')
-                    continue;
-                    int val=board[i][j]-'0';
-                    if(t[val]||rows[i][val-1]!=0||cols[j][val-1]!=0)
-                    return false;
-                    t[val]=true;
-                    rows[i][val-1]=1;
-                    cols[j][val-1]=1;
-                }
-                // cout<<endl;
+        vector<vector<int>>grid(9,vector<int>(9,0));
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')
+                continue;
+                int val=board[i][j]-'1';
+                int grididx=(i/3)*3+(j/3);
+                if(grid[grididx][val]!=0||rows[i][val]!=0||cols[j][val]!=0)
+                return false;
+                grid[grididx][val]=1;
+                rows[i][val]=1;
+                cols[j][val]=1;
             }
+            // cout<<endl;
         }
         return true;
     }
