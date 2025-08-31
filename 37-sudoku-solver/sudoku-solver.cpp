f353dffc16caefa950d0177b483589d67ccf10bf
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-08-31 11:30:22 +0530

    Time: 155 ms (90.97%) | Memory: 8.8 MB (27.06%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    void print(vector<vector<char>>&board)
+    {
+        for(auto i : board){
+            for(auto j : i)
+            cout<<j<<" ";
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+    void help(vector<vector<char>>&board,vector<vector<char>>&ans,bool rows[9][9],bool cols[9][9],bool grids[9][9],vector<pair<int,int>>&emp, int idx){
+        if(idx>=emp.size()){
+            //  print(board);
+            ans=board;
+            return ;
+        }
+        
+       
+        for(int i=1;i<=9;i++){
+            int x=emp[idx].first;
+            int y=emp[idx].second;
+            int grididx=(x/3)*3+(y/3);
+            if(rows[x][i-1]==0&&cols[y][i-1]==0&&grids[grididx][i-1]==0){
+                board[x][y]='0'+i;
+                rows[x][i-1]=true;
+                cols[y][i-1]=true;
+                grids[grididx][i-1]=true;
+                help(board,ans,rows,cols,grids,emp,idx+1);
+                board[x][y]='.';
+                rows[x][i-1]=false;
+                cols[y][i-1]=false;
+                grids[grididx][i-1]=false;
+            }
+        }
+        return ;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        bool rows[9][9];
+        bool cols[9][9];
+        bool grids[9][9];
+        vector<pair<int,int>>emp;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')
+                emp.push_back({i,j});
+                else{
+                    int val=board[i][j]-'1';
+                    rows[i][val]=true;
+                    cols[j][val]=true;
+                    int grididx=(i/3)*3+(j/3);
+                    grids[grididx][val]=true;
+                }
+            }
+        }
+        // for(int i=0;i<9;i++){
+
+        //     for(int j=0;j<9;j++)
+        //     cout<<grids[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        vector<vector<char>>temp=board;
+        help(temp,board,rows,cols,grids,emp,0);
+    }
+};
