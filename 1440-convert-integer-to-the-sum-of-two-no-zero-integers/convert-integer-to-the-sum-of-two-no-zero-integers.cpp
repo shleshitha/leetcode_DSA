7f31a71c0355dfe43304c685ddd0851d85b668ce
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-09-08 19:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.27%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    set<int>s;
     bool check (int n){
-        if(s.find(n)==s.end()){
-            while(n>0){
-                if(n%10==0)
-                return false;
-                n=n/10;
-            }
+        while(n>0){
+            if(n%10==0)
+            return false;
+            n=n/10;
         }
-       
         return true;
     }
     vector<int> getNoZeroIntegers(int n) {
-        
         for(int i=1;i<=n;i++){
-            if(check(i)){
-                s.insert(i);
-                if(check(n-i))
+            if(check(i)&&check(n-i)){
                 return {i,n-i};
             }
         }
