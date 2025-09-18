04a9539ff6ab1398e4ab9387638a7fb36b2607c9
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-09-18 14:24:52 +0530

    Time: 21 ms (64.08%) | Memory: 17.3 MB (98.30%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>>ans;
-        set<vector<long long>>exist;
+        // set<vector<long long>>exist;
+        sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
                 long long sum=nums[i]+nums[j];
                 long long rem=target-sum;
-                set<long long>s;
-                for(int k=j+1;k<n;k++){
-                    if(s.find(rem-nums[k])==s.end())
-                    s.insert(nums[k]);
-                    else{
-                        vector<long long>temp={nums[i],nums[j],nums[k],rem-nums[k]};
-                        sort(temp.begin(),temp.end());
-                        if(exist.find(temp)==exist.end()){
-                            ans.push_back({nums[i],nums[j],nums[k],int(rem-nums[k])});
-                            exist.insert(temp);
-                        }
+                int k=j+1,l=n-1;
+                while(k<l){
+                    long long sum=nums[i]+nums[j];
+                    long long sum2=nums[k]+nums[l];
+                    if(sum2==rem){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
+                    else if(sum2<rem) k++;
+                    else l--;
                 }
             }
         }
