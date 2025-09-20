ba35dc0e7187dcdd505bc4c4a83a7a2682fc08af
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-09-20 16:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (65.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -13,17 +13,10 @@ public:
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int mini=0,n=nums.size();
-        int lo=0,hi=n-1;
-        while(lo<=hi){
-            int mid = (lo+hi)/2;
-            if(nums[mid]>nums[n-1])
-            lo=mid+1;
-            else
-            hi=mid-1;
-        }
-        mini=lo;
-        cout<<mini<<endl;
-        return max(bs(nums,0,mini-1,target),bs(nums,mini,nums.size()-1,target));
+        int mini = min_element(nums.begin(),nums.end())-nums.begin();
+        int n=nums.size();
+        if(target<=nums[n-1])
+        return bs(nums,mini,nums.size()-1,target);
+        return bs(nums,0,mini-1,target);
     }
 };
