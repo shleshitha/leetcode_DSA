29eee6487eb4836b3082312454b8a75f2fcf7156
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-11-12 17:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.87%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        // Initialize index, sign, and result
-                int i = 0, sign = 1;
-                         // Use long to handle overflow
-                                 long res = 0;
+        int i=0;
+        while(i<s.size() && s[i]==' ')
+        i++;
+        if(i==s.size()) return 0;
+        long sign=1,ans=0;
+        if(s[i]=='-'){
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+')
+        i++;
+        while(i<s.size() && isdigit(s[i])){
+            ans=ans*10+(s[i]-'0');
+            long t=sign*ans;
+            if(t>INT_MAX)
+            return INT_MAX;
 
-                                         // Skip leading whitespaces
-                                                 while (i < s.size() && s[i] == ' ') i++;
+            if(t<INT_MIN)
+            return INT_MIN;
 
-                                                         // Return 0 if only spaces are found
-                                                                 if (i == s.size()) return 0;
+            
+            i++;
+        }
 
-                                                                         // Check for optional '+' or '-' sign
-                                                                                 if (s[i] == '-') {
-                                                                                             sign = -1;
-                                                                                                         i++;
-                                                                                                                 } else if (s[i] == '+') {
-                                                                                                                             i++;
-                                                                                                                                     }
-
-                                                                                                                                             // Convert characters to integer while valid digits
-                                                                                                                                                     while (i < s.size() && isdigit(s[i])) {
-                                                                                                                                                                 res = res * 10 + (s[i] - '0');
-
-                                                                                                                                                                             // Clamp to INT_MAX if overflow occurs
-                                                                                                                                                                                         if (sign * res > INT_MAX) return INT_MAX;
-
-                                                                                                                                                                                                     // Clamp to INT_MIN if underflow occurs
-                                                                                                                                                                                                                 if (sign * res < INT_MIN) return INT_MIN;
-
-                                                                                                                                                                                                                             i++;
-                                                                                                                                                                                                                                     }
-
-                                                                                                                                                                                                                                             // Return final result after applying sign
-                                                                                                                                                                                                                                                     return (int)(sign * res);
+        return sign*ans;
     }
 };
