5c6794c30c470f076e187f267579ecd6525af81b
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-11-13 08:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.16%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
+    long helper(string s,int sign, int i, long ans){
+        
+        if(i>=s.size() || (!isdigit(s[i]))) return (int)(sign*ans);
+        
+        ans=ans*10+(s[i]-'0');
+        if(sign*ans>=INT_MAX)
+        return INT_MAX;
+        if(sign*ans<=INT_MIN)
+        return INT_MIN;
+        return helper(s,sign,i+1,ans);
+
+    }
     int myAtoi(string s) {
-        int i=0;
+        int i=0,sign=1;
         while(i<s.size() && s[i]==' ')
         i++;
         if(i==s.size()) return 0;
-        long sign=1,ans=0;
-        if(s[i]=='-'){
-            sign=-1;
-            i++;
-        }
-        else if(s[i]=='+')
-        i++;
-        while(i<s.size() && isdigit(s[i])){
-            ans=ans*10+(s[i]-'0');
-            long t=sign*ans;
-            if(t>INT_MAX)
-            return INT_MAX;
-
-            if(t<INT_MIN)
-            return INT_MIN;
-
-            
-            i++;
-        }
-
-        return sign*ans;
+        if(s[i]=='-') {sign=-1;
+        i++;}
+        else if(s[i]=='+') i++;
+        return helper(s,sign,i,0);
     }
 };
