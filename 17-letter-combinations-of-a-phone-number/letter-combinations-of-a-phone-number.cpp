0256dd6c79cbcd866adc34c8f4a958ff88fb9639
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-11-13 13:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (37.28%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void helper(string digits,map<char, vector<char>> &mp, vector<string>&ans, string t,int i){
+        if(t.size()==digits.size()){
+            ans.push_back(t);
+            return;
+        }
+        for(int j=0;j<mp[digits[i]].size();j++){
+            helper(digits,mp,ans,t+mp[digits[i]][j],i+1);
+        }
+        
+    }
+    vector<string> letterCombinations(string digits) {
+        map<char, vector<char>> mp = {
+            { '2', {'a','b','c'} },
+            { '3', {'d','e','f'} },
+            { '4', {'g','h','i'} },
+            { '5', {'j','k','l'} },
+            { '6', {'m','n','o'} },
+            { '7', {'p','q','r','s'} },
+            { '8', {'t','u','v'} },
+            { '9', {'w','x','y','z'} }
+        };
+        vector<string>ans;
+        string t;
+        helper(digits,mp,ans,t,0);
+        return ans;  
+    }
+};
