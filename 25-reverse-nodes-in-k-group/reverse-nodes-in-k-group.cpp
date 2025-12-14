c4d4503bf74488b89a92ad4d49e6ae431aa1808b
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-12-14 15:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (95.50%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,35 +10,31 @@
  */
 class Solution {
 public:
-     ListNode* reversell(ListNode* head) {
-        if(!head || head->next==NULL)
-        return head;
-        ListNode* temp = reversell(head->next);
+    ListNode* revll(ListNode* head){
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* r=revll(head->next);
         head->next->next=head;
         head->next=NULL;
-        
-        return temp;
+        return r;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *dummy=new ListNode(-1);
-        ListNode* temp=head, *prev=head,*temp2=dummy;
-        int cnt=1;
-        while(temp){
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* prev=dummy, *t=head;
+        int cnt=0;
+        while(t){
+            cnt++;
             if(cnt==k){
-                ListNode* next=temp->next;
-                temp->next=NULL;
-                temp2->next=reversell(prev);
-                temp2=prev;
-                prev=next;
-                temp=next;
-                cnt=1;
+                ListNode *n=t->next;
+                t->next=NULL;
+                t=prev->next;
+                prev->next=revll(prev->next);
+                t->next=n;
+                cnt=0;
+                prev=t;
             }
-            else{
-                cnt++;
-                temp=temp->next;
-            }
+            t=t->next;
         }
-        temp2->next=prev;
         return dummy->next;
     }
 };
