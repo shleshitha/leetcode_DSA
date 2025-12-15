559171b220497e27bc6502709662787cf350e661
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-12-15 12:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (71.96%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -9,33 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class preorder{
-    stack<TreeNode*>s;
-    public:
-    preorder(TreeNode *root){
-        s.push(root);
-    }
-    bool hasnext(){
-        return !s.empty();
-    }
-    int getnext(){
-        TreeNode *n=s.top();
-        s.pop();
-        if(n->right)
-        s.push(n->right);
-        if(n->left)
-        s.push(n->left);
-        return n->val;
-    }
-};
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>ans;
         if(root==NULL) return ans;
-        preorder p(root);
-        while(p.hasnext()){
-            ans.push_back(p.getnext());
+        stack<TreeNode*>s;
+        s.push(root);
+        // preorder p(root);
+        while(!s.empty()){
+            TreeNode *n=s.top();
+            s.pop();
+            ans.push_back(n->val);
+            if(n->right)
+            s.push(n->right);
+            if(n->left)
+            s.push(n->left);
         }
         return ans;
     }
